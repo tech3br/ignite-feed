bb37c19a727cd69e7c75d0ef1b2b913577d6bfc7
tech3br <dfsdireito95@gmail.com>
2022-08-30 20:05:59 -0300

    :sparkles: feat: add header component into main app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header'
 import './styles.css'
 
 export function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
     </div>
   )
 }
