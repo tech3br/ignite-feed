5263dbc974c42b0b8e5f629386b5fee74cb99eec
tech3br <dfsdireito95@gmail.com>
2022-08-30 20:56:54 -0300

    :sparkles: feat: add new styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/fav.svg';
+
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="Logo do Ignite" />
+            <h1>Ignite Feed</h1>
         </header>
     )
 }
