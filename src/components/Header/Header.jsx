86f011e367cd45de71748fff99e758745ad925fa
tech3br <dfsdireito95@gmail.com>
2022-08-31 01:32:01 -0300

    :art: organize imports into many files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
-import styles from './Header.module.css'
-
-import igniteLogo from '../../assets/fav.svg';
+import igniteLogo from "../../assets/fav.svg";
+import styles from "./Header.module.css";
 
 export function Header() {
-    return (
-        <header className={styles.header}>
-            <img src={igniteLogo} alt="Logo do Ignite" />
-            <h1>Ignite Feed</h1>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logo do Ignite" />
+      <h1>Ignite Feed</h1>
+    </header>
+  );
 }
