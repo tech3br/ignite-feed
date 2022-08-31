fd1395999a24f990b575a0d2257603810b91ccd1
tech3br <dfsdireito95@gmail.com>
2022-08-31 01:06:12 -0300

    :sparkles: add new component into main app file and add post fake

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { Header } from './components/Header'
-import './global.css'
+import styles from "./App.module.css";
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import "./global.css";
 
 export function App() {
-
   return (
     <div className="App">
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <div>
+            <h2>Daniel Filgueira</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
+              molestiae ullam voluptate quidem ducimus error nemo sapiente
+              eveniet exercitationem facilis. Sequi maiores iusto amet sit id
+              vero nemo non a!
+            </p>
+          </div>
+          <div>
+            <h2>Daniel Filgueira</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
+              molestiae ullam voluptate quidem ducimus error nemo sapiente
+              eveniet exercitationem facilis. Sequi maiores iusto amet sit id
+              vero nemo non a!
+            </p>
+          </div>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
