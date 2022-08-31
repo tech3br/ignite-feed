ea2d47a0603f085940b86fbfc92645a197db853d
tech3br <dfsdireito95@gmail.com>
2022-08-31 00:56:12 -0300

    :sparkles: add new component sidebar with css module

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/tech3br.png"
+          alt="Profile Image"
+        />
+        <strong>Daniel Filgueira</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
