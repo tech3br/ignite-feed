b8a916c58034285cd3666f230c2122e23b5d4205
tech3br <dfsdireito95@gmail.com>
2022-09-01 00:45:35 -0300

    :sparkles: add new component into main app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import { Header } from "./components/Header/Header";
+import { Post } from "./components/Post/Post";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import "./global.css";
 
@@ -10,24 +11,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <div>
-            <h2>Daniel Filgueira</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-              molestiae ullam voluptate quidem ducimus error nemo sapiente
-              eveniet exercitationem facilis. Sequi maiores iusto amet sit id
-              vero nemo non a!
-            </p>
-          </div>
-          <div>
-            <h2>Daniel Filgueira</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-              molestiae ullam voluptate quidem ducimus error nemo sapiente
-              eveniet exercitationem facilis. Sequi maiores iusto amet sit id
-              vero nemo non a!
-            </p>
-          </div>
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
